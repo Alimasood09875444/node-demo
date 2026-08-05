9cc1913088ded0e74bf64298b9d979849bfbf1f2
Alimasood098765444 <alimasood098765444@gmail.com>
2026-08-04 22:38:27 -0700

    add if else

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 let a=19;
 let b=10;
 console.log(a+b);
+
+if(a==19){
+  console.log("if statement true");
+  
+}
+if (b==7) {
+  console.log("is statement");
+  
+}else{
+  console.log("else statement Done");
+  
+}
