4026841905eee46d2544496981c8a811e695f534
Alimasood098765444 <alimasood098765444@gmail.com>
2026-08-07 21:47:04 -0700

    Add module

--- code.js
+++ code.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const os = require('os')
+const {log,warn}=require('console')
 fs.writeFileSync("dummy.txt","Masood is good developer")
 
 console.log(os.platform());
 console.log(os.hostname());
+log("custom hock")
+warn("custom hock")
